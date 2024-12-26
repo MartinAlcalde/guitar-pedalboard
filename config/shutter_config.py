7c948e2539857917740eed8b4f06ad52bafcb7e8
Martin Alcalde <Tin_92@hotmail.es>
2024-12-26 15:42:43 -0300

    info added

--- config/shutter_config.py
+++ config/shutter_config.py
@@ -3,9 +3,15 @@ from devices.shutter import ShutterConfig
 vendor_id = 0x2717
 product_id = 0x0040
 
+# este es el shutter que tengo en el pedal
+vendor_id = 0x2717
+product_id = 0x0040
+
 # this is the tiny shutter
 #vendor_id = 0x248a
 #product_id = 0x8266
+
+
 action = '5' # Toggle command "5" from midi_config
 
 shutter_config = ShutterConfig(
